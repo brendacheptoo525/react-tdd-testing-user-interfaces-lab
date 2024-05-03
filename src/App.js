1cc2b99998b8269b7bd7d41bfc215dde4be8518e
Brenda Cheptoo <bcheptoo85@gmail.com>
2024-05-03 12:22:05 +0300

    Lab complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div>Your code here</div>;
+  return <div>
+        <h1>Hi, I'm (your name)</h1>
+        <img alt="My profile pic" src="https://via.placeholder.com/350" />
+        <h2>About Me</h2>
+        <p>I am a Software Developer</p>
+        <a href="https://github.com">GitHub</a>
+        <a href="https://linkedin.com">LinkedIn</a>
+  </div>;
 }
 
 export default App;
